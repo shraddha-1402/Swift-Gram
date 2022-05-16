8584c8109ea92032951d26ba0a976cd4aaf36710
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-16 11:47:34 +0530

    feat: theme context created

---
+++ src/context/theme-context.js
@@ -0,0 +1,65 @@
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const DarkModeContext = createContext();
+
+const DarkModeProvider = ({ children }) => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      mode,
+      toggleDarkMode: () => {
+        localStorage.setItem("theme", mode === "light" ? "dark" : "light");
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    [mode]
+  );
+
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+      primary: { main: "#E94826" },
+      ...(mode === "dark"
+        ? {
+            background: {
+              default: "#1E1F21",
+              paper: "#2A2B2D",
+            },
+          }
+        : {
+            background: {
+              default: "#F7FAFC",
+              paper: "#fcfcfc",
+            },
+          }),
+    },
+  });
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme) {
+      setMode(theme);
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
+  }, []);
+
+  return (
+    <DarkModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </DarkModeContext.Provider>
+  );
+};
+
+const useDarkMode = () => useContext(DarkModeContext);
+
+export { DarkModeProvider, useDarkMode };
