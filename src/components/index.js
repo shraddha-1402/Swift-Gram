216f3b19260cd5035c6932cbd1b25f373e997397
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-16 23:53:23 +0530

    feat: imported components

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,5 @@
 export { Navbar } from "./Navbar";
+export { LeftSidebar } from "./LeftSidebar";
+export { RightSidebar } from "./RightSidebar";
+export { Bottombar } from "./Bottombar";
+export { Middlebar } from "./Middlebar";
