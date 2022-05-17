65f3086c4a5477a034c49066bbd6bf6f87bd816b
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-17 15:28:41 +0530

    feat: homepage created

---
+++ src/features/HomePage/index.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+import { Middlebar, PostCreateCard, PostCard } from "../../components";
+
+const HomePage = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        padding: "1.5rem",
+        marginBottom: { xs: "3rem", md: "0" },
+      }}
+    >
+      <PostCreateCard />
+      <Middlebar />
+      <PostCard />
+    </Box>
+  );
+};
+
+export { HomePage };
