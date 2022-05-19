654de82954f2008da62c0964b4fd6dbeb0610f44
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-19 14:33:46 +0530

    fix: added usersReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, profileReducer } from "../features";
+import { authReducer, profileReducer, usersReducer } from "../features";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     profile: profileReducer,
+    users: usersReducer,
   },
 });
