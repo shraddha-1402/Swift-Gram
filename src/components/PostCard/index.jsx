c693bc944c54c982bcb87017bc1c8a437816c31c
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-21 08:38:33 +0530

    fix: bookmark removed from popover added to post

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import {
   Avatar,
@@ -11,11 +11,12 @@ import {
 } from "@mui/material";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
-import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import AddCommentIcon from "@mui/icons-material/AddComment";
 import ShareIcon from "@mui/icons-material/Share";
 
-import { useLikePosts } from "../../hooks";
+import { useLikePosts, useBookmarkPost } from "../../hooks";
 import { routes } from "../../constants";
 import { PostCardPopover } from "../";
 import { formatDate } from "../../utils";
@@ -23,22 +24,32 @@ import { formatDate } from "../../utils";
 const PostCard = ({ post }) => {
   const navigate = useNavigate();
   const { user: authUser } = useSelector((store) => store.auth);
-  const { users } = useSelector((store) => store.users);
+  const { users, isLoading } = useSelector((store) => store.users);
   const { postLoadingState } = useSelector((store) => store.posts);
   const [open, setOpen] = useState(false);
   const [currUser, setCurrUser] = useState([]);
   const [isLoggedInUser, setIsLoggedInUser] = useState();
   const { isLiked, handlelikes } = useLikePosts(post);
+  const { isBookmarked, handleBookmark } = useBookmarkPost(post);
+  const [showOptions, setShowOptions] = useState(false);
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    if (users.length > 0)
-      setCurrUser(users.filter((user) => user.username === post.username)[0]);
-  }, [users, post.username]);
+    if (users.length > 0) {
+      const foundUser = users.find((user) => user.username === post.username);
+      if (foundUser) setCurrUser(foundUser);
+    }
+  }, [users, authUser, post.username]);
 
   useEffect(() => {
     if (currUser.username === authUser.username) setIsLoggedInUser(true);
     else setIsLoggedInUser(false);
   }, [currUser, authUser]);
+
+  useEffect(() => {
+    if (isLoggedInUser && pathname.split("/")[1] !== "home")
+      setShowOptions(true);
+  }, [pathname, isLoggedInUser]);
 
   return (
     <Paper
@@ -78,18 +89,14 @@ const PostCard = ({ post }) => {
             </Typography>
           </Stack>
         </Stack>
-        <Box position="relative">
-          <IconButton onClick={() => setOpen(true)}>
-            <MoreHorizIcon />
-          </IconButton>
-          {open && (
-            <PostCardPopover
-              isLoggedInUser={isLoggedInUser}
-              setOpen={setOpen}
-              post={post}
-            />
-          )}
-        </Box>
+        {showOptions && (
+          <Box position="relative">
+            <IconButton onClick={() => setOpen(true)}>
+              <MoreHorizIcon />
+            </IconButton>
+            {open && <PostCardPopover setOpen={setOpen} post={post} />}
+          </Box>
+        )}
       </Stack>
       <Typography sx={{ padding: "1rem 0" }}>{post?.content}</Typography>
       <Stack direction="row" justifyContent="space-between">
@@ -102,9 +109,6 @@ const PostCard = ({ post }) => {
           </IconButton>
           <Typography>{post?.likes.likeCount}</Typography>
         </Box>
-        <IconButton>
-          <ThumbDownAltIcon />
-        </IconButton>
         <Box
           component="span"
           sx={{ display: "flex", alignItems: "center", width: "2.5rem" }}
@@ -114,6 +118,9 @@ const PostCard = ({ post }) => {
           </IconButton>
           <Typography>{post?.comments.length}</Typography>
         </Box>
+        <IconButton disabled={isLoading} onClick={handleBookmark}>
+          {isBookmarked ? <BookmarkIcon /> : <BookmarkBorderIcon />}
+        </IconButton>
         <IconButton>
           <ShareIcon />
         </IconButton>
